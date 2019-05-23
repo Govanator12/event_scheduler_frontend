a86346ef5149d6050f59f2b061b3ee0d084e4c6b
Steven Hardy <steverhardy@gmail.com>
2019-05-23 14:57:41 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header'
+import { Switch, Route } from 'react-router-dom';
+import ScheduleEvent from './views/scheduleEvent';
+import DisplayEvents from './views/displayEvents';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' render={() => <ScheduleEvent />} />
+            <Route exact path='/display' render={() => <DisplayEvents />} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
